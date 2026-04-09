da9a752bf600c59ed9c070248e1c7fda5bf6012c
ahmed-ashraf-1 <ahmed.ashraf.msq@gmail.com>
2026-04-09 20:49:50 +0200

    initial: add template assets and core pages

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AdminController extends Controller
+{
+    public function index(string $page): View
+    {
+        if (view()->exists($page)) {
+            return view($page);
+        } else {
+            return view('404');
+        }
+
+        //   return view($id);
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit(int $id): View
+    {
+        return view('404');
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+
+    public function destroy($id)
+    {
+        //
+    }
+}
